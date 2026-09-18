3943812afe489a0105c09ab8b2e75581f454fe1e
areinerc-star <a.reiner.c@gmail.com>
2026-09-19 03:13:33 +0800

    Update product catalog to 5 products (PILIPINAS singlet + 4 PR PROJECT items) and remove all placeholder items

--- lib/products.ts
+++ lib/products.ts
@@ -8,14 +8,10 @@ export interface Product {
 
 export const PRODUCTS: Product[] = [
   { name: "FROSTLINE PILIPINAS singlet", sku: "FRL-PH-TNK", price: 450, img: "/images/PILIPINAS blue.png" },
-  { name: "The Caddie Jacket [Navy]", sku: "OR_CAD_N", price: 3200, img: "/images/product-jacket.png" },
-  { name: "ORGC Traditions Crewneck [Burgundy]", sku: "OR_CRW_B", price: 2100, img: "/images/product-crewneck.png" },
-  { name: "1990s Heritage Polo", sku: "OR-HGP_B", price: 1850, img: "/images/product-polo.png" },
-  { name: "Pleated Daily Trouser [Khaki]", sku: "OR_TRO_K", price: 1800, img: "/images/product-trouser.png" },
-  { name: "Reflections T-Shirt [Black]", sku: "OR_RFL_B", price: 1000, img: "/images/product-tee.png" },
-  { name: "Heritage Rope Cap [Olive]", sku: "OR-SC_OG", price: 750, img: "/images/product-cap.png" },
-  { name: "OR Monogram T-Shirt [Black]", sku: "OR_ORMT_B", price: 1000, img: "/images/product-monogram-tee.png" },
-  { name: "Odd Ritual Classic Polo [Cream]", sku: "OR-CGP_B", price: 850, img: "/images/product-classic-polo.png" },
+  { name: "PR PROJECT Racerback Singlet", sku: "FRL-PJ-SGL", price: 450, img: "/images/PR PROJECT black  singlet (front).png" },
+  { name: "PR PROJECT Speed Suit", sku: "FRL-PJ-SPD", price: 999, img: "/images/PR PROJECT red speed suit (front)).png" },
+  { name: "PR PROJECT Midriff Crop Top", sku: "FRL-PJ-MDF", price: 399, img: "/images/PR PROJECT white midriff (front).png" },
+  { name: "PR PROJECT Crew T-Shirt", sku: "FRL-PJ-TEE", price: 550, img: "/images/PR PROJECT white tshirt (front).png" },
 ].map((p, id) => ({ ...p, id }))
 
 export const formatPrice = (n: number) =>
