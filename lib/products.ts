c0f71611cedf43db4199c0c21505dd0689f60df3
areinerc-star <a.reiner.c@gmail.com>
2026-09-19 23:56:17 +0800

    fix: standardize image filenames to web-safe paths and update product components

--- lib/products.ts
+++ lib/products.ts
@@ -7,11 +7,11 @@ export interface Product {
 }
 
 export const PRODUCTS: Product[] = [
-  { name: "FROSTLINE PILIPINAS singlet", sku: "FRL-PH-TNK", price: 450, img: "/images/PILIPINAS blue.png" },
-  { name: "PR PROJECT Racerback Singlet", sku: "FRL-PJ-SGL", price: 450, img: "/images/PR PROJECT black  singlet (front).png" },
-  { name: "PR PROJECT Speed Suit", sku: "FRL-PJ-SPD", price: 999, img: "/images/PR PROJECT red speed suit (front)).png" },
-  { name: "PR PROJECT Midriff Crop Top", sku: "FRL-PJ-MDF", price: 399, img: "/images/PR PROJECT white midriff (front).png" },
-  { name: "PR PROJECT Crew T-Shirt", sku: "FRL-PJ-TEE", price: 550, img: "/images/PR PROJECT white tshirt (front).png" },
+  { name: "FROSTLINE PILIPINAS singlet", sku: "FRL-PH-TNK", price: 450, img: "/images/pilipinas-flat.png" },
+  { name: "PR PROJECT Racerback Singlet", sku: "FRL-PJ-SGL", price: 450, img: "/images/singlet-flat.png" },
+  { name: "PR PROJECT Speed Suit", sku: "FRL-PJ-SPD", price: 999, img: "/images/speedsuit-flat.png" },
+  { name: "PR PROJECT Midriff Crop Top", sku: "FRL-PJ-MDF", price: 399, img: "/images/midriff-flat.png" },
+  { name: "PR PROJECT Crew T-Shirt", sku: "FRL-PJ-TEE", price: 550, img: "/images/tshirt-flat.png" },
 ].map((p, id) => ({ ...p, id }))
 
 export const formatPrice = (n: number) =>
